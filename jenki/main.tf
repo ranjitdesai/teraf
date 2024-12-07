315ce40ea927eed743b737b49396f35b50bc379b
ranjitdesai <ranjitdesai2105@gmail.com>
2024-12-07 13:20:59 +0530

    Update main.tf

--- jenki/main.tf
+++ jenki/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "name" {
     ami = "ami-0614680123427b75e"
     instance_type = "t2.micro"
-    key_name = "mumkey2"
+    key_name = "mumkey"
     tags = {
       name="ranjitdesai"
 
