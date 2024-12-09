e87ca16b2909af300635e32aa473a75482721f69
ranjitdesai <ranjitdesai2105@gmail.com>
2024-12-09 22:37:02 +0530

    Update main.tf
    s3 bucket and file add

--- day1-ranjit/main.tf
+++ day1-ranjit/main.tf
@@ -11,3 +11,9 @@ resource "aws_s3_bucket" "ranjit" {
     bucket = "ranjit-bucket-sdfdf"
 
 }
+resource "aws_s3_object" "playing" {
+    bucket = aws_s3_bucket.ranjit.bucket
+    source = "./file22.txt"
+    key = "file22.txt"
+  
+}
